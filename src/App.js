1344ed41288015884e7dd239f0ccfd286a16f76e
MacBook-2 <macbook-2@MacBook-2s-MacBook-Pro.local>
2020-01-22 08:26:25 -0500

    Removed React-Router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { HashRouter, Route, Link } from 'react-router-dom';
 import Projects from './Projects.js'; 
 import Contact from './Contact.js'; 
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
       <div className="App">
-        <div className="navigation">
-          {/* <div className="navigation-sub">
-            <Link to="/" className="item">Projects</Link>
-            <Link to="/contact" className="item">Contact</Link>
-          </div>
-  */}
-        </div>
+        {Projects}
       </div>
-
-      <Route exact path="/" component={Projects} />
-        <Route path="/about" component={Contact} />
-
-    </BrowserRouter>
   );
 }
 
